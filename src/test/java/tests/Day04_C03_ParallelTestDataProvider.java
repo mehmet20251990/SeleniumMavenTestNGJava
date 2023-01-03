90226964848c52ed5bd67c1174107a060b6da36b
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-03 22:17:06 +0300

    3.1 Update-2

---
+++ src/test/java/tests/Day04_C03_ParallelTestDataProvider.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Day04_C03_ParallelTestDataProvider {
+    @Test(dataProvider = "getData")
+    public void test(String name, String surname) {
+        System.out.println(name+" "+surname+" "+Thread.currentThread().getId());
+    }
+    @DataProvider(parallel = true)
+    public Object[][] getData() {
+        return new Object[][]{
+                {"yusuf", "çelik"},
+                {"yunus", "delik"},
+                {"can", "balık"}
+        };
+    }
+}
