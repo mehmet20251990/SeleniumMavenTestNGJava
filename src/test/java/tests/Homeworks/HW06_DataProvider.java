cb660cb6ac4f8dc8c97f6ade4cec6a1665d5ebba
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-05 20:44:21 +0300

    5.1 Update-1

---
+++ src/test/java/tests/Homeworks/HW06_DataProvider.java
@@ -0,0 +1,9 @@
+package tests.Homeworks;
+
+public class HW06_DataProvider {
+    //Go to URL:
+    //https://opensource demo.orangehrmlive.com/
+    //Login with negative credentilas by Data Provider.
+    //Then assert that ‘’Invalid credentials’’ is displayed.
+
+}
---
+++ src/test/java/tests/Homeworks/HW07_DataProvider2.java
@@ -0,0 +1,9 @@
+package tests.Homeworks;
+
+public class HW07_DataProvider2 {
+     /*
+Open the site http://opencart.abstracta.us/
+Using the Search function do it with Data Provider for Mac, iPad and Samsung
+     */
+
+}
