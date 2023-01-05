ef77199ce22bcaabdbab2b3808f0a03b5cf9af04
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-05 22:37:52 +0300

    5.1 Update-3

---
+++ src/test/java/tests/Day06_C02_JSUtil.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import utilities.BaseCrossBrowserTest;
+import utilities.JSUtils;
+
+public class Day06_C02_JSUtil extends BaseCrossBrowserTest {
+    @Test
+    public void test() throws InterruptedException {
+        JSUtils.driver = driver;
+
+        driver.get("https://www.amazon.com/");
+
+        JSUtils.scrollIntoViewJS( driver.findElement(By.partialLinkText("Amazon Ignite")));
+        // Debug amacli
+        Thread.sleep(4000);
+
+        JSUtils.scrollAllUpByJS();
+        Thread.sleep(4000);
+
+        JSUtils.flash( driver.findElement(By.id("twotabsearchtextbox")));
+        Thread.sleep(4000);
+    }
+}
