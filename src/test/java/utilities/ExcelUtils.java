1bd40488fb705519ded23ac6a5e089ebb396c874
mehmet20251990 <mehmet20251990@gmail.com>
2023-01-23 21:26:44 +0300

    last update

--- src/test/java/utilities/ExcelUtils.java
+++ src/test/java/utilities/ExcelUtils.java
@@ -1,6 +1,6 @@
 package utilities;
-import org.apache.poi.ss.usermodel.*;
 
+import org.apache.poi.ss.usermodel.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,14 +1,13 @@
 package utilities;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 public class ReusableMethods {
     public static String getScreenshot(WebDriver driver, String name) throws IOException {
         // Screenshot dosya ismi icin suanki tarihi string olarak aliyoruz
